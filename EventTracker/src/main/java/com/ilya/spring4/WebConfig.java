40524cf5e891143b21cd606f820729e0de4f487e
ilya-a87 <ilya.a87@gmail.com>
2015-05-01 22:56:47 +0200

    Recreated as Spring Utility Project
    The project recreated as Spring Utility Project due to problems with
    Tomcat

---
+++ EventTracker/src/main/java/com/ilya/spring4/WebConfig.java
@@ -0,0 +1,10 @@
+package com.ilya.spring4;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig {
+
+}
